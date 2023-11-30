4e1ec8bbf05908745a5f9a3da21f2577aafe7e12
ArtemovArt <68962368+ArtemovArt@users.noreply.github.com>
2023-11-30 17:11:06 +0300

    added first route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import AppRouter from "./router/AppRouter";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  );
 }
 
 export default App;
