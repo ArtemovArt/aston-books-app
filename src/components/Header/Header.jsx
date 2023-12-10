661b9bda197b8fd3a4315b4121e0286a79f97aff
ArtemovArt <68962368+ArtemovArt@users.noreply.github.com>
2023-12-10 22:05:59 +0300

    added logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import { Button } from "@mui/base";
-
+import logo from "../../images/logo.svg";
 import "./style.scss";
 
 function Header() {
   return (
     <div className="main-header-container">
       <div className="navigation-bar">
+        <img className="logo" src={logo} alt="logo" />
         <p className="book-link">Все книги</p>
         <p className="book-link">Избранное</p>
         <p className="book-link">История</p>
